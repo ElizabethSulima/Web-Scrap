7f173e5436feaa03fe9c2114cee5ae33385b0b8d
Elizaveta Sulimenko <sulimenko_eliz@mail.ru>
2023-09-11 19:27:32 +0700

    Main changed

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import json
 import requests
 import bs4
 import fake_headers
-from fake_headers import Headers
-from bs4 import BeautifulSoup
-from pprint import pprint
 
 headers_gen = fake_headers.Headers(browser='chrome', os='win')
 response = requests.get('https://spb.hh.ru/search/vacancy?text=python&area=1&area=2', headers=headers_gen.generate(), proxies={})
@@ -14,23 +11,26 @@ main_soup = bs4.BeautifulSoup(main_html, 'lxml')
 
 keywords = ['Django', 'Flask']
 
-vacancies_all = main_soup.find_all(class_='vacancy-serp-content')
+vacancies_all_list = main_soup.find(class_='vacancy-serp-content')
+vacancies_all = vacancies_all_list.find_all(class_='serp-item')
 vacancy_list = []
 
 for vacancy in vacancies_all:
     link = vacancy.find('a')['href']
-    salary = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).text
-    company = vacancy.find('h3', {'data-qa': 'bloko-header-3'}).text
+    salary = vacancy.find('span', class_='bloko-header-section-2')
+    if salary is None:
+        pay = f"Зарплата не указана"
+    else:
+        pay = salary.text
+    company = vacancy.find('a', {'data-qa': 'serp-item__title'}).text
     city = vacancy.find('div', {'data-qa': 'vacancy-serp__vacancy-address'}).text
-    vacancy_list.append({
-        'Ссылка': link,
-        'Зарплата': salary,
-        'Компания': company,
-        'Город': city})
-
-pprint(vacancy_list)
-
-
+    description = vacancy.find('a', class_='serp-item__title').text
+    if (keyword in description for keyword in keywords):
+        vacancy_list.append({
+            'Ссылка': link,
+            'Зарплата': pay,
+            'Компания': company,
+            'Город': city})
 
 with open('vacancies.json', 'w', encoding='utf-8') as file:
     json.dump(vacancy_list, file, ensure_ascii=False, indent=4)
